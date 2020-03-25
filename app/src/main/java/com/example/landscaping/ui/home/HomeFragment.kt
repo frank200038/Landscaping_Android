1ad281e412aee0c58864076093fbd0f33af19531
frank200038@gmail.com <xiaoBAI456!>
2020-03-24 23:01:46 -0400

    Add more functions

--- app/src/main/java/com/example/landscaping/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/landscaping/ui/home/HomeFragment.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 
 import com.example.landscaping.R
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var service1 : Spinner
     private lateinit var service2 : Spinner
     private lateinit var service3 : Spinner
+    private lateinit var service4 : Spinner
+    private lateinit var service5 : Spinner
     private lateinit var ft_1_0: EditText
     private lateinit var ft_1_1: EditText
     private lateinit var ft_1_2: EditText
@@ -59,9 +63,13 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        name = view.name
-        phone = view.phone
-        service1 = view.service1
+        name = view.name as EditText
+        phone = view.phone as EditText
+        service1 = view.service1 as Spinner
+        service2 = view.service2 as Spinner
+        service3 = view.service3 as Spinner
+        service4 = view.service4 as Spinner
+        service5 = view.service5 as Spinner
         ft_1_0 = view.ft1 as EditText
         ft_1_1 = view.ft12 as EditText
         ft_1_2 = view.ft13 as EditText
@@ -84,5 +92,51 @@ class HomeFragment : Fragment() {
         cost_4 = view.cost5 as EditText
         costTotal = view.totalcost as EditText
         sqftTotal = view.totalsqft as EditText
+        addAllArrays()
+        setSpinnerListener()
+        view.setOnClickListener {
+            it.hideKeyboard()
+        }
+
+    }
+
+    fun View.hideKeyboard()
+    {
+        val inputMethodManager = context!!.getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(this.windowToken,0)
+    }
+
+    fun addAllArrays()
+    {
+        val serviceToAdd = listOf<Spinner>(service1,service2,service3,service4,service5)
+        serviceArray.addAll(serviceToAdd)
+        val ftToAdd1 = listOf<EditText>(ft_1_0,ft_1_1,ft_1_2,ft_1_3,ft_1_4)
+        ftArray1.addAll(ftToAdd1)
+        val ftToAdd2 = listOf<EditText>(ft_2_0,ft_2_1,ft_2_2,ft_2_3,ft_2_4)
+        ftArray2.addAll(ftToAdd2)
+        val sqftToAdd = listOf<EditText>(sqft_0,sqft_1,sqft_2,sqft_3,sqft_4)
+        sqftArray.addAll(sqftToAdd)
+        val costToAdd = listOf<EditText>(cost_0,cost_1,cost_2,cost_3,cost_4)
+        costArray.addAll(costToAdd)
+    }
+
+    fun setSpinnerListener()
+    {
+        service1.onItemSelectedListener = this
+        service2.onItemSelectedListener = this
+        service3.onItemSelectedListener = this
+        service4.onItemSelectedListener = this
+        service5.onItemSelectedListener = this
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        if(position == 0)
+        {
+            Toast.makeText(context,"Please Select a Service",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
