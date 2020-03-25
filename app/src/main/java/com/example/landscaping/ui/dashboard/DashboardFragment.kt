059c050c2df5a6ab2387ec09034b64322f50039a
juancastro <juanfercr@hotmail.com>
2020-03-25 19:21:31 -0400

    Recycler template

--- app/src/main/java/com/example/landscaping/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/landscaping/ui/dashboard/DashboardFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.landscaping.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.recycler_view.view.*
 
 class DashboardFragment : Fragment() {
 
@@ -22,10 +25,11 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(relativeLayout, savedInstanceState)
+        val relativeLayout: RelativeLayout? = view.contact_box
     }
 }
